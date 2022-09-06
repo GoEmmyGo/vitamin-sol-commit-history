8298b8b4203ce801df33d6fa58a1391c629e8883
Emmaline <emmalinerose19@gmail.com>
2022-09-06 12:05:17 -0500

    video background permanent

--- client/src/App.js
+++ client/src/App.js
@@ -11,15 +11,15 @@ import Calculator from './components/Calculator'
 import ReturnHome from './components/ReturnHome'
 import Solution from './components/Solution'
 import './App.css'
-// import Video from './components/Video'
+import Video from './videos/high.mp4'
 // import { VideoTag } from 'react-video-tag'
 
 function App() {
 
   return (
-    <div className='video'>
-        <video id="background-video" autoPlay loop muted >
-          <source className='background-video-url' src='https://stream.mux.com/9WJ3YFNea71i1vCWhAt1XwOg6cStPoWC/high.mp4?token=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InQ5UHZucm9ZY0hQNjhYSmlRQnRHTEVVSkVSSXJ0UXhKIn0.eyJleHAiOjE2NTgxMDUxMzIsImF1ZCI6InYiLCJzdWIiOiI5V0ozWUZOZWE3MWkxdkNXaEF0MVh3T2c2Y1N0UG9XQyJ9.HGZrpbSymFxStPyaHhbtBJPkVSDm_AXvBOCOej-PCfBdP7mCn8dTg6TbTO1oJ3fhC3KJ8KLbPTUEnOHkE8xD4doHxWBNcmuuXGP1_nJ0Cqi1usCykFwCC-EeseI_ZmHW3xJWfeGDJ1qlPAsN3A2Skcj5HDJdi0oPAi9LL3x5eJmtiNuUMUYBy4pH53cqvkKuCA6SWBw62VQ3DZ3A7UEye4ddsjqd0GGncQH3AbJK5fbcZZsC6djvwoc88MqKPJhWIJ84H9fGKGmmct4y8wK6M7gfYKYf-UyXSCKWpdnHOXuxn2eLSu9eDT94TgsYEz_AcU00TgwfZZAWCrEsUfkKgg' type="video/mp4" playbackRate='0.5' />
+    <div className='bg-container'>
+      <video id="background-video" autoPlay loop muted >
+          <source className='background-video-url' src={Video} type="video/mp4" playbackrate='0.5' />
       </video>
       <Router>
         <div>
@@ -32,12 +32,6 @@ function App() {
             </Routes>
         </div>
       </Router>
-      {/* <video id="background-video" autoPlay loop muted >
-        <source src="https://stream.mux.com/9WJ3YFNea71i1vCWhAt1XwOg6cStPoWC/high.mp4?token=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InQ5UHZucm9ZY0hQNjhYSmlRQnRHTEVVSkVSSXJ0UXhKIn0.eyJleHAiOjE2NTcwNTg1MzgsImF1ZCI6InYiLCJzdWIiOiI5V0ozWUZOZWE3MWkxdkNXaEF0MVh3T2c2Y1N0UG9XQyJ9.GKVGvcbdg_WCSrtv654Ess8qXc0N-zrvFIdu56Oqn1ms7_LOAZZO3iDYwD3MA5IT9IgNohzskiDVs_1nfkGCmm4UUvjSdL9-eQZh1cTduxrBuwr3eHmxrB7GvP4N64qlbgj7M7xpxy4mWvtzVHXn1qevUYL5QOhsV2F_ktIH68mpMvQVy-7lB_fYiA_vJy-GvbH5VSJBGrr0QNujyYPQBpou6eCim8DFP2D1QxHLXLoUhA2wq9aPhXKL_eg4B34lNc3j2H3MKtEoLzSt8OdZ_HrIUEBn5ZnFjV8cNaaREHYGFSxt5OTf5wO71U9mHPkaTmg_hFs3vJCTdumbEDjUXQ" 
-        playbackrate='0.5' 
-        />
-      </video> */}
-      {/* <Video /> */}
     </div>
   )
 }
